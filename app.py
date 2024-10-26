2cb4d30f0b5c63ab1960a91f088b8084df3b21a3
Harsha Goutam Nagarkar <120675254+HarshaNagarkar@users.noreply.github.com>
2024-10-27 02:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,6 +204,6 @@ def translate():
         return jsonify({"error": str(e)}), 500
 
 # Run the Flask application
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port, debug=True)
